cb982a46ea7c03e3c6c656465fd1ebfa1c479876
Jacob Groß <kurtextrem@gmail.com>
2022-01-05 17:24:34 +0100

    speed up queue & chunk generation

--- src/_makeChunks.mjs
+++ src/_makeChunks.mjs
@@ -1,12 +1,6 @@
-const _replaceString = type => type.indexOf('string') !== -1 ? '"__par__"' : "__par__";
+const _replaceString = type => (type[1] === 's' ? '"__par__"' : '__par__')
 
-// 3 possibilities after arbitrary property:
-// - ", => non-last string property
-// - , => non-last non-string property
-// - " => last string property
-const _matchStartRe = /^(\"\,|\,|\")/;
-
-const _chunkRegex = /"\w+__sjs"/g;
+const _chunkRegex = /"\w+?__sjs"/g
 
 /**
  * @param {string} str - prepared string already validated.
@@ -15,53 +9,59 @@ const _chunkRegex = /"\w+__sjs"/g;
  * chunks used in different scenarios.
  */
 const _makeChunks = (str, queue) => {
-  const chunks = str
-      // Matching prepared properties and replacing with target with or without
-      // double quotes.
-      // => Avoiding unnecessary concatenation of doublequotes during serialization.
-      .replace(_chunkRegex, _replaceString)
-      .split("__par__"),
-    result = [];
+	const chunks = str
+			// Matching prepared properties and replacing with target with or without
+			// double quotes.
+			// => Avoiding unnecessary concatenation of doublequotes during serialization.
+			.replace(_chunkRegex, _replaceString)
+			.split('__par__'),
+		result = []
 
-  let _i;
-  const length = chunks.length;
-  for (let i = 0; i < length; ++i) {
-    const chunk = chunks[i];
+	let chunk, matchProp, nextChunk, _i, isLast, matchPropRe, firstChar, isQuote, withoutInitial
+	const length = chunks.length
+	for (let i = 0; i < length; ++i) {
+		chunk = chunks[i]
 
-    // Using dynamic regex to ensure that only the correct property
-    // at the end of the string it's actually selected.
-    // => e.g. ,"a":{"a": => ,"a":{
-    const matchProp = `("${queue[i]?.name}":(\"?))$`;
+		// Using dynamic regex to ensure that only the correct property
+		// at the end of the string it's actually selected.
+		// => e.g. ,"a":{"a": => ,"a":{
+		matchProp = `(?:"${queue[i]?.name}":(?:"?))$`
 
-    // Check if current chunk is the last one inside a nested property
-    const isLast = (_i = i + 1) === length || (
-      (_i = chunks[_i].indexOf('}')) 
-        && (_i === 0 || _i === 1)
-    );
+		// Check if current chunk is the last one inside a nested property
+		_i = i + 1
+		isLast = _i === length || ((nextChunk = chunks[_i]) && (nextChunk[0] === '{' || nextChunk[1] === '{'))
 
-    // If the chunk is the last one the `isUndef` case should match
-    // the preceding comma too.
-    const matchPropRe = new RegExp(isLast ? `(\,?)${matchProp}` : matchProp);
+		// If the chunk is the last one the `isUndef` case should match
+		// the preceding comma too.
+		matchPropRe = new RegExp(isLast ? `(?:,?)${matchProp}` : matchProp)
 
-    const withoutInitial = chunk.replace(_matchStartRe, "");
-    
-    result.push({
-      // notify that the chunk preceding the current one has not
-      // its corresponding property undefined.
-      // => This is a V8 optimization as it's way faster writing
-      // the value of a property than writing the entire property.
-      flag: false,
-      pure: chunk,
-      // Without initial part
-      prevUndef: withoutInitial,
-      // Without property chars
-      isUndef: chunk.replace(matchPropRe, ""),
-      // Only remaining chars (can be zero chars)
-      bothUndef: withoutInitial.replace(matchPropRe, ""),
-    });
-  }
+		// 3 possibilities after arbitrary property:
+		// - ", => non-last string property
+		// - , => non-last non-string property
+		// - " => last string property
+		firstChar = chunk[0]
+		isQuote = firstChar === '"'
+		if (isQuote || firstChar === ',') withoutInitial = chunk.slice(1)
+		else if (isQuote && chunk[1] === ',') withoutInitial = chunk.slice(2)
+		else withoutInitial = chunk
 
-  return result;
-};
+		result.push({
+			// notify that the chunk preceding the current one has not
+			// its corresponding property undefined.
+			// => This is a V8 optimization as it's way faster writing
+			// the value of a property than writing the entire property.
+			flag: false,
+			pure: chunk,
+			// Without initial part
+			prevUndef: withoutInitial,
+			// Without property chars
+			isUndef: chunk.replace(matchPropRe, ''),
+			// Only remaining chars (can be zero chars)
+			bothUndef: withoutInitial.replace(matchPropRe, ''),
+		})
+	}
 
-export { _makeChunks };
+	return result
+}
+
+export { _makeChunks }
--- src/_makeQueue.mjs
+++ src/_makeQueue.mjs
@@ -1,4 +1,4 @@
-import { __find, _find } from "./_utils.mjs";
+import { __find, _find } from './_utils.mjs'
 
 /**
  * @param {object} originalSchema
@@ -7,27 +7,27 @@ import { __find, _find } from "./_utils.mjs";
  * @param {array} acc
  */
 function _prepareQueue(originalSchema, queue, obj, acc = []) {
-  // this looks weird for objects, but is actually exactly what we want: object.toString() === '[object Object]'. We only want actual strings.
-  if (obj.toString().indexOf('__sjs') !== -1) { 
-    const find = __find(acc);
-    const { serializer } = find(originalSchema);
+	// this looks weird for objects, but is actually exactly what we want: object.toString() === '[object Object]'. We only want actual strings.
+	if (`${obj}`.indexOf('__sjs') !== -1) {
+		const find = __find(acc)
+		const { serializer } = find(originalSchema)
 
-    queue.push({
-      serializer,
-      find,
-      name: acc[acc.length - 1],
-    });
-    return;
-  }
+		queue.push({
+			serializer,
+			find,
+			name: acc[acc.length - 1],
+		})
+		return
+	}
 
-  // Recursively going deeper.
-  // NOTE: While going deeper, the current prop is pushed into the accumulator
-  // to keep track of the position inside of the object.
-  const keys = Object.keys(obj);
-  for (let i = 0; i < keys.length; ++i) {
-    const key = keys[i];
-    _prepareQueue(originalSchema, queue, obj[key], [...acc, key]);
-  }
+	// Recursively going deeper.
+	// NOTE: While going deeper, the current prop is pushed into the accumulator
+	// to keep track of the position inside of the object.
+	const keys = Object.keys(obj)
+	for (let i = 0; i < keys.length; ++i) {
+		const key = keys[i]
+		_prepareQueue(originalSchema, queue, obj[key], [...acc, key])
+	}
 }
 
 /**
@@ -37,9 +37,9 @@ function _prepareQueue(originalSchema, queue, obj, acc = []) {
  * => contains array stringification serializers that are lost during preparation.
  */
 const _makeQueue = (preparedSchema, originalSchema) => {
-  const queue = [];
-  _prepareQueue(originalSchema, queue, preparedSchema);
-  return queue;
-};
+	const queue = []
+	_prepareQueue(originalSchema, queue, preparedSchema)
+	return queue
+}
 
-export { _makeQueue };
+export { _makeQueue }
